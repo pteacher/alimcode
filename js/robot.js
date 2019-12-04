a0e782d3ee7c5962ddb28dcd79e31cc8a91143e9
pteacher <subfear@mail.ru>
2019-12-04 11:30:59 +0600

    executor changed

--- js/robot.js
+++ js/robot.js
@@ -18,19 +18,20 @@ app.stage.addChild(background);
 */
 
 
-var chickTexture = PIXI.Texture.fromImage('assets/chicken.png');
-var eggTexture = PIXI.Texture.fromImage('assets/egg.png');
-var foodTexture = PIXI.Texture.fromImage('assets/food.png');
+
+var goalTexture = PIXI.Texture.fromImage('assets/flag.png');
+var wallTexture = PIXI.Texture.fromImage('assets/rock.png');
+var robotTexture = PIXI.Texture.fromImage('assets/robot.png');
 
 
 var field = new PIXI.Container();
-var chick = newItem(CELL_SIZE_PX, CELL_SIZE_PX, 1, chickTexture);
+var robot;
 
 for (var x = 0; x < FIELD_SIZE_IN_CELLS; x++) {
     for (var y = 0; y < FIELD_SIZE_IN_CELLS; y++) {
         var dot = new PIXI.Graphics()
         dot.beginFill(0x9966FF);
-        dot.drawCircle(CELL_SIZE_PX + x * CELL_SIZE_PX * 2, CELL_SIZE_PX + y * CELL_SIZE_PX * 2, 3);
+        dot.drawCircle(CELL_SIZE_PX + x * CELL_SIZE_PX * 2, CELL_SIZE_PX + y * CELL_SIZE_PX * 2, 2);
         dot.endFill();
         field.addChild(dot);
     }
@@ -39,7 +40,7 @@ for (var x = 0; x < FIELD_SIZE_IN_CELLS; x++) {
 
 app.stage.addChild(field);
 getTask(0);
-app.stage.addChild(chick);
+
 
 function newItem(x, y, scale, texture) {
     var item = new PIXI.Sprite(texture);
@@ -55,27 +56,27 @@ function getTask(lvl) {
     loadJSON(function(response) {
         var data = JSON.parse(response);
         task = data[lvl].task;
-        for (var i = task.length - 1; i >= 0; i--) {
-            for (var j = task[i].length - 1; j >= 0; j--) {
-                if (task[i][j] == 1) {
-                    task[i][j] = 0;
-                }
-            }
-        }
-        task[0][0] = 1;
 
         for (var y = 0; y < FIELD_SIZE_IN_CELLS; y++) {
             for (var x = 0; x < FIELD_SIZE_IN_CELLS; x++) {
+                if (data[lvl].task[y][x] == 1) {
+                    robot = newItem(x * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, y * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, 0.5, robotTexture);
+                    
+                    robot.zOrder = -999;
+                    console.log(robot.zOrder);
+                    app.stage.addChild(robot);
+                }
                 if (data[lvl].task[y][x] == 2) {
-                    console.log(x + " " + y);
-                    var egg = newItem(x * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, y * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, 0.5, eggTexture);
-                    app.stage.addChild(egg);
+
+                    var goal = newItem(x * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, y * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, 0.5, goalTexture);
+                    goal.zOrder = 0;
+                    console.log(goal.zOrder);
+                    app.stage.addChild(goal);
                 }
 
                 if (data[lvl].task[y][x] == 3) {
-                    console.log(x + " " + y);
-                    var food = newItem(x * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, y * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, 0.5, foodTexture);
-                    app.stage.addChild(food);
+                    var wall = newItem(x * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, y * (CELL_SIZE_PX * 2) + CELL_SIZE_PX, 0.5, wallTexture);
+                    app.stage.addChild(wall);
                 }
                 
             }
@@ -90,13 +91,13 @@ function checkTask(argument) {
 function getRandInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function move() {
     var cmd = function () {
-        chick.x += Math.cos(chick.rotation) * CELL_SIZE_PX * 2;
-        chick.y += Math.sin(chick.rotation) * CELL_SIZE_PX * 2;
+        robot.x += Math.cos(robot.rotation) * CELL_SIZE_PX * 2;
+        robot.y += Math.sin(robot.rotation) * CELL_SIZE_PX * 2;
     }
     cmds.push(cmd);
 }
@@ -104,31 +105,33 @@ function move() {
 
 function turnLeft() {
     var cmd = function() {
-        chick.rotation -= Math.PI / 2;
+        robot.rotation -= Math.PI / 2;
     }
     cmds.push(cmd);
 }
 
 function turnRight() {
     var cmd = function() {
-        chick.rotation += Math.PI / 2;
+        robot.rotation += Math.PI / 2;
     }
     cmds.push(cmd);
 }
 
-function layEgg() {
+function laygoal() {
     
 }
 
-function getFood() {
+function getwall() {
     
 }
 
 
 function reset() {
-    chick.x = CELL_SIZE_PX;
-    chick.y = CELL_SIZE_PX;
-    chick.rotation = 0;
+    /*
+    robot.x = CELL_SIZE_PX;
+    robot.y = CELL_SIZE_PX;
+    */
+    robot.rotation = 0;
 }
 
 
